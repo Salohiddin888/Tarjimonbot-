ae12edebc8b9c5c871e980883b757e4ad677e596
Salohiddin888 <jalilovsalohiddin04@gmail.com>
2025-07-12 21:08:14 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,20 @@
-from flask import Flask, request
-from telegram import Bot, Update
-from telegram.ext import CommandHandler, MessageHandler, filters, ApplicationBuilder
+from telegram import Update, Bot
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 from deep_translator import GoogleTranslator
-import os
 
 TOKEN = "8148077100:AAGu5yAI0JgB2dYvWY9idjQAYVWATjvuBq8"
-bot = Bot(token=TOKEN)
-app = Flask(__name__)
 
-async def start(update: Update, context):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Salom! Matn yuboring va men uni tarjima qilaman.")
 
-async def translate(update: Update, context):
+async def translate(update: Update, context: ContextTypes.DEFAULT_TYPE):
     original = update.message.text
     translated = GoogleTranslator(source='auto', target='uz').translate(original)
     await update.message.reply_text(translated)
 
-application = ApplicationBuilder().token(TOKEN).build()
-application.add_handler(CommandHandler("start", start))
-application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, translate))
-
 if __name__ == '__main__':
-    application.run_polling()
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, translate))
+    app.run_polling()
+    
